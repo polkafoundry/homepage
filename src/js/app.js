f37f44300920604d1679792eda50d69b8f2dff4b
Tran Bao Huy <huy23121994@gmail.com>
2020-05-03 01:04:46 +0700

    update

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,22 @@
-$(document).ready(function() {
-  // $('[data-toggle="collapse"]').click(function() {
-  //   $(this).toggleClass('active')
-  // })
+$(document).ready(function () {
   $('.collapse').on('show.bs.collapse', function () {
     $(this).parent().addClass('active');
   })
   $('.collapse').on('hide.bs.collapse', function () {
     $(this).parent().removeClass('active');
   })
+
+  setInterval(() => {
+    updateText();
+  }, 3000);
+
+  var updateText = (function () {
+    var texts = ['Practical', 'Efficient', 'Frictionless'];
+    var index = 1;
+    return function () {
+      $('.home__title span').text(texts[index++]);
+      index = index == texts.length ? 0 : index
+    }
+  })();
+
 })
