8977175b24d98b131f3ea408673e4457fef1fb71
Tran Bao Huy <huy23121994@gmail.com>
2020-05-05 01:16:35 +0700

    add animation

--- src/js/app.js
+++ src/js/app.js
@@ -19,12 +19,32 @@ $(document).ready(function () {
     }
   })();
 
-  new Swiper ('.swiper-container', {
+  new Swiper('.swiper-container', {
     navigation: {
       nextEl: '.swiper-next',
       prevEl: '.swiper-prev',
     },
+    on: {
+      slideChange: function () {
+        setTimeout(() => {
+          var img = $('.swiper-slide-active').data('image');
+          console.log(img)
+          $('.swiper-image img').hide().attr('src', img).fadeIn();
+        }, 0)
+      },
+    }
 
+  })
+
+  $(window).scroll(function() {
+    $('.animate').each(function() {
+      var offset = $(this).offset().top;
+      var animateType = $(this).data('animate');
+      console.log(animateType)
+      if( !$(this).hasClass('animated') && (offset - window.innerHeight + 80) <= window.pageYOffset) {
+        $(this).addClass('animated ' + animateType);
+      }
+    })
   })
 
 })
