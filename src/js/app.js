7b1092722596a64e7c2d5f5150635abc38e760ee
Tran Bao Huy <huy23121994@gmail.com>
2020-05-08 01:21:06 +0700

    update

--- src/js/app.js
+++ src/js/app.js
@@ -16,8 +16,8 @@ $(document).ready(function () {
     var index = 0;
     return function () {
       var i = ++index % texts.length
-      const text = texts[i]+ ' Blockchain'
-      $('.home__title span').fadeOut(function() {
+      const text = texts[i] + ' Blockchain'
+      $('.home__title span').fadeOut(function () {
         $(this).text(text).fadeIn()
       })
 
@@ -36,23 +36,37 @@ $(document).ready(function () {
       slideChange: function () {
         setTimeout(() => {
           var img = $('.swiper-slide-active').data('image');
-          console.log(img)
-          $('.swiper-image img').hide().attr('src', img).fadeIn();
+          var $img = $('.swiper-image img');
+          var $loading = $('.swiper-image .loading');
+          $img.attr('src', img);
+          var count = 1;
+          var interval = setInterval(() => {
+            count++;
+            if (count > 5) $loading.show();
+            if ($img[0].complete) {
+              clearInterval(interval);
+              $loading.hide();
+            }
+          }, 100);
         }, 0)
       },
     }
 
   })
 
-  $(window).scroll(function() {
-    $('.animate').each(function() {
+  $(window).scroll(function () {
+    $('.animate').each(function () {
       var offset = $(this).offset().top;
       var animateType = $(this).data('animate');
-      console.log(animateType)
-      if( !$(this).hasClass('animated') && (offset - window.innerHeight + 80) <= window.pageYOffset) {
+      if (!$(this).hasClass('animated') && (offset - window.innerHeight + 80) <= window.pageYOffset) {
         $(this).addClass('animated ' + animateType);
       }
     })
   })
 
 })
+
+function scrollToAnchor(id){
+  var $el = $(id);
+  $('html,body').animate({scrollTop: $el.offset().top},'slow');
+}
