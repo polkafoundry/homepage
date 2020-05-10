f53e7c5062625c4740a5055446e48a981e72f503
Tran Bao Huy <huy23121994@gmail.com>
2020-05-10 17:28:35 +0700

    update

--- src/js/app.js
+++ src/js/app.js
@@ -6,26 +6,67 @@ $(document).ready(function () {
     $(this).parent().removeClass('active');
   })
 
-  setTimeout(() => {
-    updateText();
-  }, 3000);
+  $('#myModal').on('hidden.bs.modal', function (e) {
+    $(this).find('iframe').removeAttr('src');
+    updateText.play();
+  })
+
+  $('#myModal').on('show.bs.modal', function (e) {
+    var src = $(this).data('video');
+    $(this).find('iframe').attr('src', src);
+    updateText.stop();
+  })
+
+  $('#subscribe').submit(function (e) {
+    var $this = $(this);
+    $this.find('.error').text('')
+    e.preventDefault();
+    var url = $this.attr('action');
+    $.ajax({
+      type: "GET", 
+      url: url,
+      data: $this.serialize(),
+      dataType: 'json',
+      contentType: "application/json; charset=utf-8",
+      error: function (err) { alert("Could not connect to the registration server."); },
+      success: function (data) {
+        var $error = $this.find('.error');
+        if (data.result != "success") {
+          $error.addClass('text-danger').removeClass('text-success').text(data.msg)
+        } else {
+          $error.addClass('text-success').removeClass('text-danger').text('Success!')
+        }
+      }
+    });
+  })
 
   var updateText = (function () {
     var texts = ['Practical', 'Efficient', 'Frictionless'];
-    var timeout = 3000
+    var timeout = 1000;
     var index = 0;
-    return function () {
-      var i = ++index % texts.length
-      const text = texts[i] + ' Blockchain'
-      $('.home__title span').fadeOut(function () {
-        $(this).text(text).fadeIn()
-      })
+    var timeoutFunc = null;
+    return {
+      stop: function () {
+        clearTimeout(timeoutFunc);
+      },
+      play: function () {
+        var i = ++index % texts.length
+        const text = texts[i] + ' Blockchain'
 
-      var round = Math.floor(index / texts.length)
-      timeout = Math.min(Math.floor(3000 * (1 + round / 2)), 6000)
-      setTimeout(updateText, timeout)
+        var round = Math.floor(index / texts.length)
+        timeout = Math.min(Math.floor(3000 * (1 + round / 2)), 6000)
+        timeoutFunc = setTimeout(function () {
+          updateText.play();
+          $('.home__title span').fadeOut(function () {
+            $(this).text(text).fadeIn()
+          })
+        }, timeout)
+      }
     }
   })();
+
+  updateText.play();
+
 
   new Swiper('.swiper-container', {
     navigation: {
@@ -66,7 +107,7 @@ $(document).ready(function () {
 
 })
 
-function scrollToAnchor(id){
+function scrollToAnchor(id) {
   var $el = $(id);
-  $('html,body').animate({scrollTop: $el.offset().top},'slow');
+  $('html,body').animate({ scrollTop: $el.offset().top }, 'slow');
 }
